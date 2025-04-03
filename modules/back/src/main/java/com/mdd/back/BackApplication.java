dfd8470f559110492a17c8224bbcb411da5f51da
cpminipc <cpierres@hotmail.com>
2025-04-03 08:49:25 +0200

    Config : docker-compose.yml ; base Postgresql 17 R2DBC ; flyway (utilise jdbc) ; Initialisation de la DB et ajout de la table users

--- modules/back/src/main/java/com/mdd/back/BackApplication.java
+++ modules/back/src/main/java/com/mdd/back/BackApplication.java
@@ -1,11 +1,14 @@
 package com.mdd.back;
 
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.flywaydb.core.Flyway;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BackApplication.class, args);
     }
