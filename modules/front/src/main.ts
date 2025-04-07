61e396ef87afc2a5032581caa22d26148ab3e4dd
cpminipc <cpierres@hotmail.com>
2025-04-07 20:13:21 +0200

    config front : mise en place theme deeppurple (avec exemple provisoire)

--- modules/front/src/main.ts
+++ modules/front/src/main.ts
@@ -1,6 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes'; // Importez vos routes ici
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideAnimations(), // Important pour les animations Material
+    provideRouter(routes)
+  ]
+}).catch(err => console.error(err));
