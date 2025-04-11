f2c93caa7b208c828e265326df41be6601db3c74
cpminipc <cpierres@hotmail.com>
2025-04-11 11:11:31 +0200

    front: header (gestion isHeaderVisible globale)

--- modules/front/src/app/app.component.ts
+++ modules/front/src/app/app.component.ts
@@ -1,14 +1,47 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {MatButton, MatButtonModule} from '@angular/material/button';
+import {Component, OnInit} from '@angular/core';
+import {NavigationEnd, Router, RouterOutlet} from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
+//import {MessagesComponent} from './shared/components/messages/messages.component';
+import {HeaderComponent} from './shared/components/header/header.component';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatButtonModule],
+  imports: [RouterOutlet, MatButtonModule, HeaderComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'front';
+export class AppComponent implements OnInit {
+  title = 'MDD';
+  isHeaderVisible: boolean = false;
+
+  // Constante pour les routes où le header est caché
+  private readonly hiddenHeaderRoutes: string[] = ['/', '/home'];
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    //this.setHeaderVisibility(this.router.url);
+    this.subscribeToRouterEvents();
+  }
+
+  // Abonne aux événements de navigation du Router
+  private subscribeToRouterEvents(): void {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.setHeaderVisibility(event.url);
+      }
+    });
+  }
+
+  // Modifie la visibilité du header
+  private setHeaderVisibility(url: string): void {
+    this.isHeaderVisible = !this.isRouteHidden(url);
+  }
+
+  // Vérifie si la route fait partie des routes cachant le header
+  private isRouteHidden(url: string): boolean {
+    return this.hiddenHeaderRoutes.includes(url);
+  }
 }
---
+++ modules/front/src/app/shared/components/header/header.component.ts
@@ -0,0 +1,63 @@
+import {Component, HostListener, OnDestroy, OnInit} from '@angular/core';
+import {MatToolbar} from '@angular/material/toolbar';
+import {RouterLink} from '@angular/router';
+import {MatMenu, MatMenuItem, MatMenuTrigger} from '@angular/material/menu';
+import {MatButtonModule, MatIconButton} from '@angular/material/button';
+import {MatIcon} from '@angular/material/icon';
+import {Subscription} from 'rxjs';
+import {SessionService} from '../../services/session-service.service';
+import {NgIf} from '@angular/common';
+
+const MOBILE_MAX_WIDTH = 768; // Détermine largeur max pour écrans mobiles
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  imports: [
+    MatToolbar,
+    RouterLink,
+    MatMenuItem,
+    MatIconButton,
+    MatIcon,
+    MatMenu,
+    MatButtonModule,
+    MatMenuTrigger,
+    NgIf
+  ],
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent implements OnInit, OnDestroy {
+  showRightMenu: boolean = false;
+  private sessionSubscription: Subscription | undefined;
+  isMobile: boolean = false;
+
+  constructor(private sessionService: SessionService) {}
+
+  ngOnInit(): void {
+    // Gestion de la souscription pour le statut de connexion
+    this.sessionSubscription = this.sessionService.$isLogged().subscribe({
+      next: (isLogged) => this.showRightMenu = isLogged
+    });
+
+    // Détection initiale de la taille de l'écran
+    this.updateIsMobile();
+  }
+
+  @HostListener('window:resize')
+  onResize(): void {
+    this.updateIsMobile();
+  }
+
+  /**
+   * Met à jour le flag indiquant si l'écran est de type mobile
+   * @private
+   */
+  private updateIsMobile(): void {
+    this.isMobile = window.innerWidth <= MOBILE_MAX_WIDTH;
+  }
+
+  ngOnDestroy(): void {
+    // Nettoyage de la souscription pour éviter les fuites mémoire
+    this.sessionSubscription?.unsubscribe();
+  }
+}
