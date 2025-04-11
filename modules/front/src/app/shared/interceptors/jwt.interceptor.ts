6871793d0a6f994465e3ba1040d9613179a6f9e6
cpminipc <cpierres@hotmail.com>
2025-04-11 18:18:23 +0200

    front: configuration de jwtInterceptor compatible avec Angular 19

--- modules/front/src/app/shared/interceptors/jwt.interceptor.ts
+++ modules/front/src/app/shared/interceptors/jwt.interceptor.ts
@@ -1,19 +1,14 @@
-import { HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
-import { Injectable } from "@angular/core";
+import { HttpHandler, HttpInterceptorFn } from '@angular/common/http';
 
-@Injectable({ providedIn: 'root' })
-export class JwtInterceptor implements HttpInterceptor {
-  constructor() {}
-
-  public intercept(request: HttpRequest<any>, next: HttpHandler) {
-    const token = localStorage.getItem('token');
-    if (token) {
-      request = request.clone({
-        setHeaders: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-    }
-    return next.handle(request);
+// Définit l'intercepteur comme une fonction compatible avec Angular 19
+export const jwtInterceptor: HttpInterceptorFn = (request, next) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    request = request.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
   }
-}
+  return next(request);
+};
