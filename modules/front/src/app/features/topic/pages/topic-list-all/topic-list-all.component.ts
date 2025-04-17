45d821522aebba7a3b64c3554d24dc38f5d07caa
cpminipc <cpierres@hotmail.com>
2025-04-17 07:02:54 +0200

    front : initialisation topic-list (component) et topic-list-all (page)

---
+++ modules/front/src/app/features/topic/pages/topic-list-all/topic-list-all.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-topic-list-all',
+  imports: [],
+  templateUrl: './topic-list-all.component.html',
+  styleUrl: './topic-list-all.component.scss'
+})
+export class TopicListAllComponent {
+
+}
