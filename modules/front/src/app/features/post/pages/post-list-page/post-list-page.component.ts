4adbd6d117fd5dfbbebd566f3f472cdaef32225b
cpminipc <cpierres@hotmail.com>
2025-04-24 15:53:13 +0200

    feat-post : front - Sur page `post-list-page`, la liste `Trier par` doit afficher tri par défaut qui est `Date (récent d'abord)`

--- modules/front/src/app/features/post/pages/post-list-page/post-list-page.component.ts
+++ modules/front/src/app/features/post/pages/post-list-page/post-list-page.component.ts
@@ -33,7 +33,7 @@ export class PostListPageComponent implements OnInit {
   topics: TopicStatsDto[] = [];
   posts: PostDto[] = [];
   selectedTopicId: string = 'subscribed';
-  sortCriteria: string | null = null; // Par défaut: posts des topics auxquels l'utilisateur est abonné
+  sortCriteria: string = 'date'; // Par défaut: tri par date (récent d'abord)
   cols: number = 2; // Nb cols par défaut sur grand écran
 
   constructor(private router: Router,
