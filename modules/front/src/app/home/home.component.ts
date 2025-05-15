fe284bb4c1d9082215eb91fb7e97e010722a42d3
cpminipc <cpierres@hotmail.com>
2025-05-15 22:58:56 +0200

    feat-home-center : mieux centrer le home sur écran mobile (et mobile landscape)

--- modules/front/src/app/home/home.component.ts
+++ modules/front/src/app/home/home.component.ts
@@ -8,7 +8,7 @@ import {Router} from '@angular/router';
     MatButtonModule
   ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.scss'
 })
 export class HomeComponent {
 
